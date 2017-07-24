0cd1e762df96bdd4f1de4e3267060e222c906caa
jtcy0123 <jasmin0123@hotmail.com>
2017-07-24 10:50:23 +0800

    layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723120923) do
+ActiveRecord::Schema.define(version: 20170724022600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20170723120923) do
     t.string "link"
     t.string "photos"
     t.bigint "mood_id"
-    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["mood_id"], name: "index_contents_on_mood_id"
   end
 
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 20170723120923) do
     t.string "emotion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
